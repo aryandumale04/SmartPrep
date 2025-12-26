613404e27175a2b3225823fae7a18bd0e5938a6c
Aryan Dumale <aryandumale04@gmail.com>
2025-12-26 12:00:11 +0530

    Update AuthMiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -11,7 +11,6 @@ const protect = async(req, res, next) => {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.userId).select("-password");
             next();
-
         }else{
            return  res.status(401).json({ message: "Not authorized, no token" });
 
