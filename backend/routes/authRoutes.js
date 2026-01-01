0c183e5ca4364def3bfdd3beb3cfa46912225d2b
Aryan Dumale <aryandumale04@gmail.com>
2026-01-01 20:08:12 +0530

    Update auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-const express =  require("express");
+const express = require("express");
 const { registerUser, loginUser, getUserProfile } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/uploadMiddleware")
